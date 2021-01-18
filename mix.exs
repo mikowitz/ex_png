9fd3ba8b20b1c0d57293f61d4e0b95a49171df81
Michael Berkowitz <michael.berkowitz@gmail.com>
2021-01-18 13:51:14 -0500

    Update mix.exs to allow testing against earlier elixir versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExPng.MixProject do
     [
       app: :ex_png,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
